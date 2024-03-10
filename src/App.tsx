fb8bc55ae453ed29375234d22484d518d3b3a8c3
Hi <huydao1008@gmail.com>
2024-03-10 22:49:41 +0700

    Add new files and update existing files

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,27 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
-import { Button } from "./components/ui/button";
+import { useState } from 'react'
+import viteLogo from '/vite.svg'
+import { Button } from './components/ui/button'
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} alt="Vite logo" />
+        <a href='https://vitejs.dev' target='_blank'>
+          <img src={viteLogo} alt='Vite logo' />
         </a>
       </div>
       <h1>Vite + React</h1>
       <div>
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
+        <Button onClick={() => setCount((count) => count + 1)}>count is {count}</Button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
       <p>Click on the Vite and React logos to learn more</p>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
